27acf990c1f5c367f6006947b327d96ecbc24b6f
Stevie <xxlennygamerxx@gmail.com>
2020-10-13 21:58:48 -0700

    Edit template

---
+++ src/main/java/io/github/steviegt6/steviesadditions/SteviesAdditions.java
@@ -0,0 +1,10 @@
+package io.github.steviegt6.steviesadditions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SteviesAdditions implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/io/github/steviegt6/steviesadditions/mixin/SteviesAdditionsMixin.java
@@ -0,0 +1,8 @@
+package io.github.steviegt6.steviesadditions.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TitleScreen.class)
+public class SteviesAdditionsMixin {
+}
